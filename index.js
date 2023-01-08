db107bb4b9f1f398a28c4ecfdbcb5007c632f8bf
aad61404 <asd61404@gmail.com>
2023-01-09 01:30:37 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,120 @@
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+
+canvas.width = 1024
+canvas.height = 576
+
+const gravity = 0.3
+
+class Sprite {
+    constructor({ position, imageSrc }) {
+        this.position = position
+        this.image = new Image()
+        this.image.src = imageSrc
+    }
+
+    draw() {
+        if (!this.image) return
+        c.drawImage(this.image, this.position.x, this.position.y)
+    }
+
+    update() {
+        this.draw()
+    }
+}
+
+class Player {
+    constructor(position) {
+        this.position = position
+        this.velocity = {
+            x: 0,
+            y: 1
+        }
+        this.height = 100;
+    }
+
+    draw() {
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x, this.position.y, 100, 100)
+    }
+
+    update() {
+        this.draw()
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
+        if (this.position.y + this.velocity.y + this.height < canvas.height) {
+            this.velocity.y += gravity
+        } else {
+            this.velocity.y = 0;
+        }
+    }
+}
+
+const player = new Player({
+    x: 0,
+    y: 0,
+});
+const player2 = new Player({
+    x: 300,
+    y: 100,
+});
+
+let y = 100
+
+const keys = {
+    d: {
+        pressed: false,
+    },
+    a: {
+        pressed: false,
+    }
+}
+
+const background = new Sprite({
+    position: {
+        x: 0,
+        y: 0,
+    },
+    imageSrc: './img/background.png',
+})
+
+function animate() {
+    window.requestAnimationFrame(animate)
+    c.fillStyle = 'white'
+    c.fillRect(0, 0, canvas.width, canvas.height)
+    player.update()
+    player2.update()
+    player.velocity.x = 0
+    if (keys.d.pressed) {
+        player.velocity.x = 1
+    } else if (keys.a.pressed) {
+        player.velocity.x = -1
+    }
+}
+
+animate()
+
+window.addEventListener('keydown', (event) => {
+    switch (event.key) {
+        case 'd':
+            keys.d.pressed = true
+            break;
+        case 'a':
+            key.a.pressed = true
+        case 'w':
+            player.velocity.y = -10
+            break;
+        case 's':
+            player.velocity.y = 10;
+            break;
+    }
+})
+window.addEventListener('keyup', (event) => {
+    switch (event.key) {
+        case 'd':
+            keys.d.pressed = false
+            break;
+        case 'a':
+            key.a.pressed = false
+    }
+})
